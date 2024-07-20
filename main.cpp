febbfad2fa65f10eaf41904911eea00f020ad3b7
ChrisWoo0443 <chefwoodini@gmail.com>
2024-07-19 17:57:50 -0700

    added image and xml

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+#include <opencv2/opencv.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/objdetect.hpp>
+
+using namespace cv;
 using namespace std;
+
 int main() {
-    cout << "hello world" << endl;
-    return 1;
+
 }
