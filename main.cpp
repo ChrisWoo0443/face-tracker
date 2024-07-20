8b5af79f3449c4da65900ae1486e69b612ecf0eb
ChrisWoo0443 <chefwoodini@gmail.com>
2024-07-19 18:32:56 -0700

    added rectangle and face label for face tracking

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
 #include <opencv2/imgproc.hpp>
-#include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/objdetect.hpp>
 
@@ -12,14 +11,32 @@ int main() {
     VideoCapture cap(1);
     Mat img;
 
+    //load haar cascade for frontal face
+    CascadeClassifier faceCascade;
+    faceCascade.load("Resources/haarcascade_frontalface_default.xml");
+
+    if(faceCascade.empty()) {
+        cout << "XML not properly loaded" << endl;
+    }
+
     while(true) {
         cap.read(img);
 
+        vector<Rect> faces;
+        faceCascade.detectMultiScale(img, faces, 1.1, 10);
+
+        //put rectangle with face label around face
+        for(int i = 0; i < faces.size(); i++) {
+            putText(img, "Face", {faces[i].x, faces[i].y-10}, FONT_HERSHEY_PLAIN, 2, Scalar(0,255,0), 1);
+            rectangle(img, faces[i].tl(), faces[i].br(), Scalar(255, 0, 255), 3);
+        }
+
         imshow("Webcam", img);
-        char quitKey = (char) waitKey(1);
-        if(quitKey == 27) {
+
+        //close video with esc key, ascii value of 27
+        char escKey = (char) waitKey(1);
+        if(escKey == 27) {
             break;
         }
     }
-
 }
